c9200eb733d9b65a0a8a7ca5d801faeea8b62337
Robert <Robert.kralofsky@rkos.at>
2016-06-18 11:32:28 +0200

    only package the tool directory of opencover nuget package

--- TFSExtension/build.bat
+++ TFSExtension/build.bat
@@ -15,7 +15,7 @@ cd ..
 echo Building RunOpenCoverTask
 xcopy /S /I .\packages\NUnit.ConsoleRunner.3.2.1  .\RunNunitWithOpenCover\dist\NUnit.ConsoleRunner.3.2.1
 xcopy /S /I .\packages\NUnit.Extension.NUnitProjectLoader.3.2.1 .\RunNunitWithOpenCover\dist\NUnit.Extension.NUnitProjectLoader.3.2.1
-xcopy /S /I .\packages\OpenCover.4.6.519  .\RunNunitWithOpenCover\dist\OpenCover.4.6.519
+xcopy /S /I .\packages\OpenCover.4.6.519\tools  .\RunNunitWithOpenCover\dist\OpenCover.4.6.519\tools
 xcopy /S /I /Exclude:xcopy1.excl .\packages\ReportGenerator.2.4.5.0  .\RunNunitWithOpenCover\dist\ReportGenerator.2.4.5.0 
 xcopy .\RunNunitWithOpenCover\ResultDummy.xml .\RunNunitWithOpenCover\dist\
 xcopy /S /I .\TFSRestTool\bin\Debug .\RunNunitWithOpenCover\dist\RestHelper
